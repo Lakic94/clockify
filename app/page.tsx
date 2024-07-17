2608392f5e69e454512f7eabc176d9468c38c069
Nikola Lakic <nilakic94@gmail.com>
2024-07-17 14:33:38 +0200

    clientiD

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function Home() {
   return (
-    <GoogleOAuthProvider
-      clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? ""}
-    >
+    <GoogleOAuthProvider clientId={"128986968164-fj8hgsi825motncp3aotbaurfla0hjh4.apps.googleusercontent.com"}>
       <div className="clockify-card cl-border-bottom cl-mb-4 h-20">
         <ProbaTest />
       </div>
